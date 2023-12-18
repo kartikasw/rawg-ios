3612ff256c5d57e4abebafce6cb7d110c941b438
kartikasw <kartikasariwidagdo@gmail.com>
2023-12-19 05:17:57 +0700

    Initial Commit

---
+++ RawgApp/RawgAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RawgAppApp.swift
+//  RawgApp
+//
+//  Created by Kartika Sari on 19/12/23.
+//
+
+import SwiftUI
+
+@main
+struct RawgAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
